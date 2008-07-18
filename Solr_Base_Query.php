f211e74d532a7f3a7e8877eb62f52bf5a608bbc0
Thomas Seidl <remus@gmx.net>
2008-07-18 23:53:19 +0000

    Part of a first shot at a true Solr backend for Views. Doesn't work yet, but at least everything else seems to work fine. Added apachesolr_views_query class that will, once finished, replace views' query class in Solr's Views backend. What is already there should be working, some methods that need to be called when the view is built are missing. Also abstracted the Solr_Base_Query::get_breadcrumb() method somewhat so it doesn't only work on search pages.

--- Solr_Base_Query.php
+++ Solr_Base_Query.php
@@ -153,8 +153,9 @@ class Solr_Base_Query {
     ksort($this->_fields);
 
     $progressive_crumb = array();
-    // TODO: Don't know if hardcoding this is going to come back to bite.
-    $base = 'search/'. arg(1). '/';
+    
+    // This should also work for paths other than search/%/*
+    $base = substr($_GET['q'], 0, strrpos($_GET['q'], '/')) . '/';
 
     foreach ($this->_fields as $field) {
       $progressive_crumb[] = Solr_Base_Query::make_field($field);
