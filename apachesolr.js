ca9c8b1d4699171b712c973706442cc64255dd06
Robert Douglass <robertdouglass@5449.no-reply.drupal.org>
2009-11-03 12:35:59 +0000

    #621922 by robertDouglass make the 'Show more' js more robust.

--- apachesolr.js
+++ apachesolr.js
@@ -3,12 +3,12 @@
 Drupal.behaviors.apachesolr = function(context) {
   $('.apachesolr-hidden-facet').hide();
   $('<a href="#" class="apachesolr-showhide"></a>').text(Drupal.t('Show more')).click(function() {
-    if ($(this).prev().find('.apachesolr-hidden-facet:visible').length == 0) {
-      $(this).prev().find('.apachesolr-hidden-facet').show();
+    if ($(this).parent().find('.apachesolr-hidden-facet:visible').length == 0) {
+      $(this).parent().find('.apachesolr-hidden-facet').show();
       $(this).text(Drupal.t('Show fewer'));
     }
     else {
-      $(this).prev().find('.apachesolr-hidden-facet').hide();
+      $(this).parent().find('.apachesolr-hidden-facet').hide();
       $(this).text(Drupal.t('Show more'));
     }
     return false;
