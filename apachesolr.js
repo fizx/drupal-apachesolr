2842721dcf5e39340d4848a07d0373649ca24d5e
Claudiu Cristea <clau.cristea@gmail.com>
2010-02-09 08:44:01 +0000

    #708812 by claudiu.cristea: "Show more" link is missed from facet blocks.

--- apachesolr.js
+++ apachesolr.js
@@ -2,14 +2,14 @@
 
 Drupal.behaviors.apachesolr = function(context) {
   $('.apachesolr-hidden-facet').hide();
-  $('<a href="#" class="apachesolr-showhide"></a>').text(Drupal.t('Show more')).click(function() {
+  $('<a href="#" class="apachesolr-showhide"></a>').text(Drupal.settings.apachesolr.showMore).click(function() {
     if ($(this).parent().find('.apachesolr-hidden-facet:visible').length == 0) {
       $(this).parent().find('.apachesolr-hidden-facet').show();
-      $(this).text(Drupal.t('Show fewer'));
+      $(this).text(Drupal.settings.apachesolr.showFewer);
     }
     else {
       $(this).parent().find('.apachesolr-hidden-facet').hide();
-      $(this).text(Drupal.t('Show more'));
+      $(this).text(Drupal.settings.apachesolr.showMore);
     }
     return false;
   }).appendTo($('.block-apachesolr_search:has(.apachesolr-hidden-facet), .block-apachesolr:has(.apachesolr-hidden-facet)'));
