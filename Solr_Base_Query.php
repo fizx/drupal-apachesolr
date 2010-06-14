ef14dae31da90f76d83914684665aefc2b99dbcc
Peter Wolanin <pwolanin@49851.no-reply.drupal.org>
2010-06-14 02:51:30 +0000

    #824534 backport from 6.x-2.x of method to replace keywords in query object.

--- Solr_Base_Query.php
+++ Solr_Base_Query.php
@@ -209,6 +209,18 @@ class Solr_Base_Query implements Drupal_Solr_Query_Interface {
     $this->parse_filters();
   }
 
+  function get_keys() {
+    return $this->keys;
+  }
+
+  function set_keys($keys) {
+    $this->keys = $keys;
+  }
+
+  public function remove_keys() {
+    $this->keys = '';
+  }
+
   public function add_subquery(Drupal_Solr_Query_Interface $query, $fq_operator = 'OR', $q_operator = 'AND') {
     $this->subqueries[$query->id] = array('#query' => $query, '#fq_operator' => $fq_operator, '#q_operator' => $q_operator);
   }
