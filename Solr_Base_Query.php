86cd2aa665700c1be415915c2be2b94415dc1f01
James McKinney <james@slashpoundbang.com>
2011-03-24 18:00:53 -0400

    Follow-up to #885950: parse_filters() causes any manually added filters to be lost

--- Solr_Base_Query.php
+++ Solr_Base_Query.php
@@ -181,8 +181,8 @@ class SolrBaseQuery implements DrupalSolrQueryInterface {
     return FALSE;
   }
 
-  public function add_filter($field, $value, $exclude = FALSE, $callbacks = array()) {
-    $filter = array('#exclude' => $exclude, '#name' => $field, '#value' => trim($value), '#callbacks' => $callbacks);
+  public function add_filter($name, $value, $exclude = FALSE, $callbacks = array()) {
+    $filter = array('#exclude' => $exclude, '#name' => $name, '#value' => trim($value), '#callbacks' => $callbacks);
     // Record the addition.
     $this->fields_added[] = $filter;
     // Add to the public list of filters.
